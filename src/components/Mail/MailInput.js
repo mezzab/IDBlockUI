044ecd7689dd79d29ed5537f30b8bfa29b59ff06
tiincho94 <martinbec1@hotmail.com>
2018-06-24 21:06:09 -0300

    Agregamos input mail y validacion

--- src/components/Mail/MailInput.js
+++ src/components/Mail/MailInput.js
@@ -4,9 +4,28 @@ import { AsyncStorage } from "react-native";
 import { Text, View, StyleSheet } from "react-native";
 import { Pages, Keys } from "../../utils/constants";
 import TextButton from "../Button";
+import { TextInput } from "react-native-gesture-handler";
 
 export class MailInput extends React.Component {
-  state = { entityJSON: "" };
+  state = {
+    email :'',
+    entityJSON: "" };
+
+  validarMail = (text) => {
+    console.log(text);
+    let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ;
+    if(reg.test(text) === false)
+    {
+    console.log("Email is Not Correct");
+    this.setState({email:text})
+    return false;
+      }
+    else {
+      this.setState({email:text})
+      console.log("Email is Correct");
+      console.log(this.state.email);
+    }
+    }
 
   render() {
     AsyncStorage.getItem(Keys.EntityJSON).then(entityJSON =>
@@ -18,6 +37,15 @@ export class MailInput extends React.Component {
         <Text style={{ padding: 25, height: 300 }}>
           {this.state.entityJSON}
         </Text>
+
+        <TextInput
+          ref = "mail"
+          style={{height: 40}}
+          placeholder="tumail@mail.com"
+          onChangeText={(text) => this.validarMail(text)}
+          value={this.state.email}
+        />
+
         <TextButton
           margin="10px 0  10px 0"
           value="Go back to home"

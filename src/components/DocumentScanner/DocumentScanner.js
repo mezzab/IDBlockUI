33c9d62101ea8a7b8915b467c98fd78dd9ade611
Franco Scagnetti <franscagnetti@hotmail.com>
2018-07-08 21:51:08 +0000

    fix camera

--- src/components/DocumentScanner/DocumentScanner.js
+++ src/components/DocumentScanner/DocumentScanner.js
@@ -2,9 +2,35 @@ import React from "react";
 import { Container, InputText, InputField } from "../shared";
 import TextButton from "../Button";
 import { Pages, Keys, IconsType } from "../../utils/constants";
-
 import { Text, View, TouchableOpacity } from 'react-native';
 import { Camera, Permissions } from 'expo';
+import { Vibration } from 'react-native';
+import styled from "styled-components";
+
+export const StyledView = styled.View`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const TopLimits = styled.Text`
+  color: #fff;
+  font-size: 18px;
+  font-weight: bold;
+  text-align: center;
+  align-self: center;
+  margin-top: 5%;
+  margin-left: -12%;
+`;
+
+const BottomLimits = styled.Text`
+  color: #fff;
+  font-size: 18px;
+  font-weight: bold;
+  text-align: center;
+  margin-top: 20%;
+  margin-left: -12%;
+`;
 
 
 
@@ -20,63 +46,85 @@ export class DocumentScanner extends React.Component {
   }
 
   takePicture = () => {
-    console.log('TSD1')
     if (this.camera) {
-      console.log('TSD2')
-        this.camera.takePictureAsync()
-            .then(data => console.log('TSD3'))
+      Vibration.vibrate(30);
+      this.camera.takePictureAsync()
+        .then(data => console.log('TSD3')) //TODO guardar foto
     }
-}
+  }
 
-    render() {
-      const { hasCameraPermission } = this.state;
-      if (hasCameraPermission === null) {
-        return <View />;
-      } else if (hasCameraPermission === false) {
-        return <Text>No access to camera</Text>;
-      } else {
-        return (
-          <View style={{ flex: 1 }}>
-            <Camera ref={ref => this.camera = ref} style={{ flex: 1 }} type={this.state.type}>
-            
-              <View
+  render() {
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <Camera ref={ref => this.camera = ref} style={{ flex: 1 }} type={this.state.type}
+          >
+            <StyledView>
+              <Text
                 style={{
-                  flex: 1,
-                  backgroundColor: 'transparent',
-                  flexDirection: 'row',
+                  fontFamily: "msyi",
+                  fontSize: 30,
+                  alignSelf: "center",
+                  padding: 10,
+                  paddingTop: 7,
+                  color: '#fff',
+                }}
+              > Picture your frontal DNI</Text>
+              <TopLimits>┌                                                                                     ┐</TopLimits>
+              <BottomLimits>└                                                                                     ┘</BottomLimits>
+            </StyledView>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                flexDirection: 'row',
+              }}>
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                }}
+                onPress={() => {
+                  this.setState({
+                    type: this.state.type === Camera.Constants.Type.back
+                      ? Camera.Constants.Type.front
+                      : Camera.Constants.Type.back,
+                  });
                 }}>
-                <TouchableOpacity
-                  style={{
-                    flex: 0.1,
-                    alignSelf: 'flex-end',
-                    alignItems: 'center',
-                  }}
-                  onPress={() => {
-                    this.setState({
-                      type: this.state.type === Camera.Constants.Type.back
-                        ? Camera.Constants.Type.front
-                        : Camera.Constants.Type.back,
-                    });
-                  }}>
-                  <Text
-                    style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
-                    {' '}Flip{' '}
-                  </Text>
-                  
-                </TouchableOpacity>  
-              </View>
 
-                  <TextButton
-                    margin="10px 0  10px 0"
-                    value="AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
-                    onPress={() => this.takePicture()}
-                    />
-              
-            </Camera>
-          </View>
-        );
+
+              </TouchableOpacity>
+            </View>
+            <TouchableOpacity
+              style={{
+                borderWidth: 1,
+                borderColor: 'rgba(0,0,0,0.2)',
+                alignItems: 'center',
+                alignSelf: 'center',
+                justifyContent: 'center',
+                width: 70,
+                height: 70,
+                backgroundColor: '#fff',
+                borderRadius: 100,
+              }}
+              onPress={() => this.takePicture()}
+            >
+
+            </TouchableOpacity>
+
+
+
+          </Camera>
+        </View>
+      );
     }
   }
 }
-  
+
 
